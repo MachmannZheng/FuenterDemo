af126fb2d0319d240d0b744505aa98630b1f9cbc
MachmannZheng <18651125503@163.com>
2024-01-09 22:17:49 +0800

    complete sync mutex feature

--- main/main.go
+++ main/main.go
@@ -8,5 +8,5 @@ import (
 
 func main() {
 	fmt.Println("-- start the main function --")
-	syncT.SynctestRaceCondition()()
+	syncT.SynctestRaceCondition()
 }
--- syncTest/syncmutex.go
+++ syncTest/syncmutex.go
@@ -1,7 +1,32 @@
 package synctest
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type User struct {
+	depositAmout float32
+	sync.Mutex
+}
+
+var BALANCE float32 = 12000
 
 func SynctestRaceCondition() {
 	fmt.Println("-- execute sync test Race Condition function --")
+	var wg sync.WaitGroup
+	user := User{depositAmout: 1200}
+	for i := 0; i < 10; i++ {
+		wg.Add(3)
+		go user.deposit(&wg)
+		go user.deposit(&wg)
+		go user.deposit(&wg)
+	}
+}
+
+func (u *User) deposit(wg *sync.WaitGroup) {
+	u.Lock()
+	BALANCE += u.depositAmout
+	u.Unlock()
+	wg.Done()
 }
